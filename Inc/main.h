727bec26f1b7485fcf2b2840e002357f6f0c5354
Bruno Montanari <bruno.montanari@st.com>
2017-11-28 13:51:28 -0200

    Add files via upload

---
+++ Inc/main.h
@@ -0,0 +1,236 @@
+/**
+  ******************************************************************************
+  * File Name          : main.hpp
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  * This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * Copyright (c) 2017 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+  /* Includes ------------------------------------------------------------------*/
+
+/* Includes ------------------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define NCS_MEMS_SPI_Pin GPIO_PIN_0
+#define NCS_MEMS_SPI_GPIO_Port GPIOC
+#define MEMS_INT1_Pin GPIO_PIN_1
+#define MEMS_INT1_GPIO_Port GPIOC
+#define MEMS_INT2_Pin GPIO_PIN_2
+#define MEMS_INT2_GPIO_Port GPIOC
+#define B1_Pin GPIO_PIN_0
+#define B1_GPIO_Port GPIOA
+#define EXT_RESET_Pin GPIO_PIN_5
+#define EXT_RESET_GPIO_Port GPIOC
+#define I2C2_SCL_Pin GPIO_PIN_10
+#define I2C2_SCL_GPIO_Port GPIOB
+#define I2C2_SDA_Pin GPIO_PIN_11
+#define I2C2_SDA_GPIO_Port GPIOB
+#define SPI2_SCK_Pin GPIO_PIN_13
+#define SPI2_SCK_GPIO_Port GPIOB
+#define SPI2_MISO_Pin GPIO_PIN_14
+#define SPI2_MISO_GPIO_Port GPIOB
+#define SPI2_MOSI_Pin GPIO_PIN_15
+#define SPI2_MOSI_GPIO_Port GPIOB
+#define LD3_Pin GPIO_PIN_6
+#define LD3_GPIO_Port GPIOC
+#define LD6_Pin GPIO_PIN_7
+#define LD6_GPIO_Port GPIOC
+#define LD4_Pin GPIO_PIN_8
+#define LD4_GPIO_Port GPIOC
+#define LD5_Pin GPIO_PIN_9
+#define LD5_GPIO_Port GPIOC
+#define USBF4_DM_Pin GPIO_PIN_11
+#define USBF4_DM_GPIO_Port GPIOA
+#define USBF4_DP_Pin GPIO_PIN_12
+#define USBF4_DP_GPIO_Port GPIOA
+#define SWDIO_Pin GPIO_PIN_13
+#define SWDIO_GPIO_Port GPIOA
+#define SWCLK_Pin GPIO_PIN_14
+#define SWCLK_GPIO_Port GPIOA
+
+/* ########################## Assert Selection ############################## */
+/**
+  * @brief Uncomment the line below to expanse the "assert_param" macro in the 
+  *        HAL drivers code
+  */
+/* #define USE_FULL_ASSERT    1U */
+
+/* USER CODE BEGIN Private defines */
+//all according to usb.org
+#define KEY_CTRL	0x01
+#define KEY_SHIFT	0x02
+#define KEY_ALT		0x04
+#define KEY_GUI		0x08
+#define KEY_LEFT_CTRL	0x01
+#define KEY_LEFT_SHIFT	0x02
+#define KEY_LEFT_ALT	0x04
+#define KEY_LEFT_GUI	0x08
+#define KEY_RIGHT_CTRL	0x10
+#define KEY_RIGHT_SHIFT	0x20
+#define KEY_RIGHT_ALT	0x40
+#define KEY_RIGHT_GUI	0x80
+
+#define KEY_A		4
+#define KEY_B		5
+#define KEY_C		6
+#define KEY_D		7
+#define KEY_E		8
+#define KEY_F		9
+#define KEY_G		10
+#define KEY_H		11
+#define KEY_I		12
+#define KEY_J		13
+#define KEY_K		14
+#define KEY_L		15
+#define KEY_M		16
+#define KEY_N		17
+#define KEY_O		18
+#define KEY_P		19
+#define KEY_Q		20
+#define KEY_R		21
+#define KEY_S		22
+#define KEY_T		23
+#define KEY_U		24
+#define KEY_V		25
+#define KEY_W		26
+#define KEY_X		27
+#define KEY_Y		28
+#define KEY_Z		29
+#define KEY_1		30
+#define KEY_2		31
+#define KEY_3		32
+#define KEY_4		33
+#define KEY_5		34
+#define KEY_6		35
+#define KEY_7		36
+#define KEY_8		37
+#define KEY_9		38
+#define KEY_0		39
+#define KEY_ENTER	40	
+#define KEY_ESC		41
+#define KEY_BACKSPACE	42	
+#define KEY_TAB		43
+#define KEY_SPACE	44	
+#define KEY_MINUS	45
+#define KEY_EQUAL	46
+#define KEY_LEFT_BRACE	47
+#define KEY_RIGHT_BRACE	48
+#define KEY_BACKSLASH	49
+#define KEY_NUMBER	50
+#define KEY_SEMICOLON	51
+#define KEY_QUOTE	52
+#define KEY_TILDE	53
+#define KEY_COMMA	54
+#define KEY_PERIOD	55
+#define KEY_SLASH	56
+#define KEY_CAPS_LOCK	57
+#define KEY_F1		58
+#define KEY_F2		59
+#define KEY_F3		60
+#define KEY_F4		61
+#define KEY_F5		62
+#define KEY_F6		63
+#define KEY_F7		64
+#define KEY_F8		65
+#define KEY_F9		66
+#define KEY_F10		67
+#define KEY_F11		68
+#define KEY_F12		69
+#define KEY_PRINTSCREEN	70		
+#define KEY_SCROLL_LOCK	71		
+#define KEY_PAUSE	72
+#define KEY_INSERT	73	
+#define KEY_HOME	74
+#define KEY_PAGE_UP	75
+#define KEY_DELETE	76
+#define KEY_END		77
+#define KEY_PAGE_DOWN	78	
+#define KEY_RIGHT	79
+#define KEY_LEFT	80
+#define KEY_DOWN	81
+#define KEY_UP		82
+#define KEY_NUM_LOCK	83
+#define KEYPAD_SLASH	84	
+#define KEYPAD_ASTERIX	85	
+#define KEYPAD_MINUS	86	
+#define KEYPAD_PLUS	87	
+#define KEYPAD_ENTER	88	
+#define KEYPAD_1	89
+#define KEYPAD_2	90
+#define KEYPAD_3	91	
+#define KEYPAD_4	92	
+#define KEYPAD_5	93	
+#define KEYPAD_6	94	
+#define KEYPAD_7	95	
+#define KEYPAD_8	96	
+#define KEYPAD_9	97	
+#define KEYPAD_0	98		
+#define KEYPAD_PERIOD	99	
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+void _Error_Handler(char *, int);
+
+#define Error_Handler() _Error_Handler(__FILE__, __LINE__)
+#ifdef __cplusplus
+}
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+#endif /* __MAIN_H */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
